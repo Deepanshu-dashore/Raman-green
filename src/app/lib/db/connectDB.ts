9d70455f3d758263818528b250ed2289ff376062
Deepanshu-dashore <dashd9396@gmail.com>
2026-05-08 12:32:07 +0530

    intizatial project

---
+++ src/app/lib/db/connectDB.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+export const connectDB = async (): Promise<void> => {
+    if (connection.isConnected) {
+        return;
+    }
+    if (mongoose.connection.readyState >= 1) {
+        return;
+    }
+    const connectionString = process.env.MONGODB_URL;
+    if(!connectionString){
+        throw new Error("MONGODB_URL is not defined");
+    }
+    try {
+        const db = await mongoose.connect(connectionString);
+        connection.isConnected = db.connections[0].readyState;
+        console.log("Raman-Green-Database connected");
+    } catch (error) {
+        console.error("Raman-Green-Database connection error:", error);
+        throw error;
+    }
+}
